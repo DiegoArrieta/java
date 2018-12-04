26e0ca921028c8b4f6d23440264bc316e015115f
Diego Arrieta <darrieta.lnx@gmail.com>
2018-12-03 21:58:40 -0300

    Nuevas Funciones

--- src/unidad5_clase26112018/Conexion.java
+++ src/unidad5_clase26112018/Conexion.java
@@ -8,24 +8,27 @@ package unidad5_clase26112018;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author zeuss
  */
 public class Conexion {
-    public static void connect(){
-    String url = "org.apache.derby.jdbc.ClientDriver";
-    String user = "alumno704";
-    String pass = "12345";
-    System.out.println("Conectando...");
-    try(Connection connection = DriverManager.getConnection(url, user,pass)){
-    System.out.println("Conectado!!");
-
-    }catch(SQLException e){
-    System.out.println(e.getMessage());
-    }
     
-    }
+    public Connection conectar(){
+        Connection cn=null;
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            cn = DriverManager.getConnection("jdbc:derby://localhost:1527/bd2704", "alumno704", "12345");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+        return cn;
+        }
     
 }
